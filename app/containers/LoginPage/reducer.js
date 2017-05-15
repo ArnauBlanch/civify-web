4598cb59527413ca54d81859249dae35c7e78175
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-15 20:08:23 +0200

    isAuthenticated added + routes #181

--- app/containers/LoginPage/reducer.js
+++ app/containers/LoginPage/reducer.js
@@ -7,21 +7,24 @@
 import { fromJS } from 'immutable';
 import {
   SENDING_REQUEST,
-  LOGIN_SUCCESS,
+  // LOGIN_SUCCESS,
   LOGIN_FAILED,
 } from './constants';
 
 const initialState = fromJS({
   currentlySending: false,
   loginError: undefined,
+  // isAuthenticated: localStorage.getItem('auth_token') !== null,
 });
 
 function loginPageReducer(state = initialState, action) {
   switch (action.type) {
     case SENDING_REQUEST:
       return state.set('currentlySending', action.sending);
-    case LOGIN_SUCCESS:
-      return state.set('loginError', undefined);
+    /* case LOGIN_SUCCESS:
+      return state
+        .set('loginError', undefined)
+        .set('isAuthenticated', true); */
     case LOGIN_FAILED:
       return state.set('loginError', action.error);
     default:
