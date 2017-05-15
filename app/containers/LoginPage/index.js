76b134e3345745df4e4bf5972eb7d2afd935c4d2
Arnau Blanch Cortès <arnaubc@gmail.com>
2017-05-15 17:19:00 +0200

    #181: Login funciona

--- app/containers/LoginPage/index.js
+++ app/containers/LoginPage/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router';
@@ -13,9 +13,47 @@ import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import makeSelectLoginPage from './selectors';
 import messages from './messages';
-import paperStyle from '../../paper.style';
+import LoginForm from '../../components/LoginForm';
+import { loginRequest } from './actions';
 
-export class LoginPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+export class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getErrorMessage = this.getErrorMessage.bind(this);
+  }
+
+  getErrorMessage() {
+    const { loginError } = this.props.loginState;
+    if (loginError === 'Invalid credentials') {
+      return <FormattedMessage {...messages.wrongPassword} />;
+    } else if (loginError === 'User not exists') {
+      return <FormattedMessage {...messages.userDoesNotExist} />;
+    } else if (loginError === 'No auth token in localStorage') {
+      return <FormattedMessage {...messages.thereWasAnError} />;
+    } else if (loginError === 'Not a business or admin') {
+      return <FormattedMessage {...messages.noPrivileges} />;
+    }
+    return undefined;
+  }
+
+  isEmail(text) {
+    return text && /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g.test(text);
+  }
+
+  handleSubmit(values) {
+    this.setState({ open: true });
+    const firstField = values.get('usernameOrEmail');
+    const user = { password: values.get('password') };
+    if (this.isEmail(firstField)) {
+      user.email = firstField;
+    } else {
+      user.username = firstField;
+    }
+    this.props.dispatch(loginRequest(user));
+  }
+
   render() {
     return (
       <div>
@@ -26,11 +64,23 @@ export class LoginPage extends React.Component { // eslint-disable-line react/pr
           ]}
         />
         <Paper
-          style={paperStyle}
+          style={{
+            textAlign: 'center',
+            padding: 35,
+            width: '100%',
+          }}
           zDepth={2}
         >
           <h4 style={{ margin: 0 }}><FormattedMessage {...messages.signIn} /></h4>
-          <br /><br /><br />
+          <LoginForm
+            onSubmit={this.handleSubmit}
+          />
+          <br />
+          { this.getErrorMessage() &&
+            <strong
+              style={{ color: 'rgb(244, 67, 54)' }}
+            >{this.getErrorMessage()}</strong>}
+          <br /><br />
           <FormattedMessage {...messages.notRegisteredBusiness} /><br />
           <FlatButton
             label={this.props.intl.formatMessage(messages.register)}
@@ -44,12 +94,13 @@ export class LoginPage extends React.Component { // eslint-disable-line react/pr
 }
 
 LoginPage.propTypes = {
-  // dispatch: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
   intl: intlShape.isRequired,
+  loginState: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
-  LoginPage: makeSelectLoginPage(),
+  loginState: makeSelectLoginPage(),
 });
 
 function mapDispatchToProps(dispatch) {
--- app/containers/LoginPage/sagas.js
+++ app/containers/LoginPage/sagas.js
@@ -1,11 +1,67 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { take, call, put, fork } from 'redux-saga/effects';
+import sha256 from 'js-sha256';
+import { push } from 'react-router-redux';
+import { LOGIN_REQUEST, USER_INFO_REQUEST } from './constants';
+import { sendingRequest, userInfoRequest, userInfoFailed, loginSuccess, loginFailed } from './actions';
+import request from '../../utils/request';
 
-// Individual exports for testing
-export function* defaultSaga() {
-  // See example in containers/HomePage/sagas.js
+export function* userInfo() {
+  while (true) {
+    yield take(USER_INFO_REQUEST);
+    try {
+      const response = yield call(request, '/me', 'GET', undefined, true);
+      const body = yield response.json();
+      if (response.status === 200) {
+        if (body.kind === 'business' || body.kind === 'admin') {
+          localStorage.setItem('user_token', body.user_auth_token);
+          if (body.kind === 'business') {
+            yield put(push('/')); // change
+          } else {
+            yield put(push('/')); // change
+          }
+          yield put(loginSuccess());
+        } else {
+          localStorage.removeItem('auth_token');
+          yield put(loginFailed('Not a business or admin'));
+        }
+      } else {
+        yield put(userInfoFailed());
+        yield put(loginFailed(body.message));
+      }
+    } catch (error) {
+      yield put(loginFailed(error.message));
+    }
+  }
+}
+
+export function* login() {
+  while (true) {
+    const loginRequest = yield take(LOGIN_REQUEST);
+    const { user } = loginRequest;
+    user.password = sha256(user.password);
+
+    yield put(sendingRequest(true));
+    const response = yield call(request, '/login', 'POST', user, false);
+    if (response.status === 200 || response.status === 401 || response.status === 404) {
+      const body = yield response.json();
+      if (response.status === 200 && body.auth_token) {
+        localStorage.setItem('auth_token', body.auth_token);
+        yield put(userInfoRequest());
+      } else if ((response.status === 401 && body.message === 'Invalid credentials')
+        || (response.status === 404 && body.message === 'User not exists')) {
+        yield put(loginFailed(body.message));
+      }
+    }
+    yield put(sendingRequest(false));
+  }
+}
+
+export function* root() {
+  yield fork(login);
+  yield fork(userInfo);
 }
 
 // All sagas to be loaded
 export default [
-  defaultSaga,
+  root,
 ];
