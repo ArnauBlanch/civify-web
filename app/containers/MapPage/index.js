17f2b0b4bc63a1b21cc933d3ccbe0fec37ab093b
MSI Barcelona <j.espasa.cusachs@accenture.com>
2017-05-16 13:48:02 +0200

    Moved map to MapPage

--- app/containers/MapPage/index.js
+++ app/containers/MapPage/index.js
@@ -6,24 +6,69 @@
 
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import Helmet from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
+// import { FormattedMessage } from 'react-intl';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
 import { createStructuredSelector } from 'reselect';
-
+// import Helmet from 'react-helmet';
 import makeSelectMapPage from './selectors';
-import messages from './messages';
+// import messages from './messages';
+import CustomMarker from '../../components/CustomMarker';
 
-export class MapPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+const SimpleMap = withGoogleMap((props) => (
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: 41.390205, lng: 2.154007 }}
+  >
+    {props.markers.map((marker) => (
+      <CustomMarker
+        position={marker.position}
+        category={marker.category}
+      >
+      </CustomMarker>
+    ))}
+  </GoogleMap>
+));
+
+
+class MapPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [{
+        position: {
+          lat: 41.390205,
+          lng: 2.154007,
+        },
+        category: 'road_signs',
+      }, {
+        position: {
+          lat: 41.990205,
+          lng: 2.854007,
+        },
+        category: 'suggestion',
+      }, {
+        position: {
+          lat: 41.590205,
+          lng: 2.454007,
+        },
+        category: 'street_furniture',
+      },
+      ],
+    };
+  }
+
   render() {
     return (
-      <div>
-        <Helmet
-          title="MapPage"
-          meta={[
-            { name: 'description', content: 'Description of MapPage' },
-          ]}
+      <div className="App">
+        <SimpleMap
+          containerElement={
+            <div style={{ height: 'calc(100vh - 50px)', width: '100vw' }} />
+          }
+          mapElement={
+            <div style={{ height: '100%', width: '100%', position: 'absolute' }} />
+          }
+          markers={this.state.markers}
         />
-        <FormattedMessage {...messages.header} />
       </div>
     );
   }
