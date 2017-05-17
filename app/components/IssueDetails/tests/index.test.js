1dc6a30f0b1074d7fef156792cd856e273d47231
dsegoviat <david.segovia@est.fib.upc.edu>
2017-05-17 23:46:40 +0200

    Component IssueDetails generated (blank)

---
+++ app/components/IssueDetails/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import IssueDetails from '../index';
+
+describe('<IssueDetails />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
