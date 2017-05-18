e0a101f5878edfd90f55ac7bb451a7c3b902d114
Arnau Blanch Cortès <arnaubc@gmail.com>
2017-05-18 03:41:23 +0200

    #153 acabada + testejada

--- app/components/RewardCard/index.js
+++ app/components/RewardCard/index.js
@@ -4,20 +4,48 @@
 *
 */
 
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Card, CardTitle, CardMedia } from 'material-ui';
-// import { FormattedMessage } from 'react-intl';
-// import messages from './messages';
+import API_URL from '../../api';
+import coinsIcon from '../../images/coins.svg';
 
-class RewardCard extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class RewardCard extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="mdl-cell mdl-cell--4-col">
-        <Card>
-          <CardMedia>
-            <img alt="Reward" src="http://img2.thejournal.ie/article/3275421/river?version=3275440&width=1340" />
+        <Card onClick={() => console.log(this.props.reward.award_auth_token)}>
+          <CardMedia
+            style={{
+              height: 220,
+              width: '100%',
+              overflow: 'hidden',
+              position: 'relative',
+            }}
+          >
+            <div
+              style={{ height: 220, backgroundColor: '#eee' }}
+            >
+              <img
+                alt="Reward"
+                src={`${API_URL}/${this.props.reward.picture.med_url}`}
+                height={220}
+              /></div>
           </CardMedia>
-          <CardTitle title="10% descompte en sessions golfa" subtitle="creada fa 20 dies" />
+          <CardTitle
+            title={this.props.reward.title}
+            subtitle={<span>
+              <img alt="Coins" src={coinsIcon} height={25} />&nbsp;&nbsp;{this.props.reward.price}
+            </span>}
+            titleStyle={{
+              fontWeight: 'bold',
+              fontSize: 20,
+              lineHeight: 1.3,
+              marginBottom: 10,
+              height: 50,
+              overflow: 'hidden',
+            }}
+            subtitleStyle={{ fontSize: 20 }}
+          />
         </Card>
       </div>
     );
@@ -25,7 +53,7 @@ class RewardCard extends React.Component { // eslint-disable-line react/prefer-s
 }
 
 RewardCard.propTypes = {
-
+  reward: PropTypes.object.isRequired,
 };
 
 export default RewardCard;
--- app/containers/RewardsPage/actions.js
+++ app/containers/RewardsPage/actions.js
@@ -5,11 +5,19 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  GET_REWARDS_REQUEST,
+  GET_REWARDS_SUCCESS,
+  GET_REWARDS_FAILED,
 } from './constants';
 
-export function defaultAction() {
-  return {
-    type: DEFAULT_ACTION,
-  };
+export function getRewardsRequest() {
+  return { type: GET_REWARDS_REQUEST };
+}
+
+export function getRewardsSuccess(rewards) {
+  return { type: GET_REWARDS_SUCCESS, rewards };
+}
+
+export function getRewardsFailed() {
+  return { type: GET_REWARDS_FAILED };
 }
