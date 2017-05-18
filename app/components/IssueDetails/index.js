b9092fc58829033954f0dc49282f71b57884b01c
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-18 04:32:16 +0200

    Map changed from stateless function to PureComponent + some refactoring

--- app/components/IssueDetails/index.js
+++ app/components/IssueDetails/index.js
@@ -5,30 +5,22 @@
 */
 
 import React, { PropTypes } from 'react';
-import { Drawer, AppBar, MenuItem } from 'material-ui';
-import { Link } from 'react-router';
-
-// import styled from 'styled-components';
-
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { Drawer } from 'material-ui';
+// import { FormattedMessage } from 'react-intl';
+// import messages from './messages';
 
 class IssueDetails extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { issue } = this.props;
     return (
       <Drawer
         docked={false}
         open={this.props.open}
         onRequestChange={this.props.toggleDrawer}
       >
-        <AppBar
-          title={'Issue clicada'}
-          onLeftIconButtonTouchTap={this.props.toggleDrawer}
-        />
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <MenuItem onTouchTap={this.props.toggleDrawer} >
-          </MenuItem>
-        </Link>
+        <div style={{ marginTop: 50 }}>
+          ISSUE AUTH TOKEN: { issue ? issue.issue_auth_token : '(cap)' }
+        </div>
       </Drawer>
     );
   }
@@ -37,6 +29,7 @@ class IssueDetails extends React.PureComponent { // eslint-disable-line react/pr
 IssueDetails.propTypes = {
   open: PropTypes.bool.isRequired,
   toggleDrawer: PropTypes.func.isRequired,
+  issue: PropTypes.object,
 };
 
 export default IssueDetails;
