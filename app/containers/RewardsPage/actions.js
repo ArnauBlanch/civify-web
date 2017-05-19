1bd9c036779b97fa04da5b94383ae345021aba1f
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-19 20:29:49 +0200

    #171 sense tests

--- app/containers/RewardsPage/actions.js
+++ app/containers/RewardsPage/actions.js
@@ -8,6 +8,8 @@ import {
   GET_REWARDS_REQUEST,
   GET_REWARDS_SUCCESS,
   GET_REWARDS_FAILED,
+  DELETE_REWARD_REQUEST,
+  DELETE_REWARD_SUCCESS,
 } from './constants';
 
 export function getRewardsRequest() {
@@ -20,4 +22,12 @@ export function getRewardsSuccess(rewards) {
 
 export function getRewardsFailed() {
   return { type: GET_REWARDS_FAILED };
+}
+
+export function deleteRewardRequest(rewardID) {
+  return { type: DELETE_REWARD_REQUEST, rewardID };
+}
+
+export function deleteRewardSuccess() {
+  return { type: DELETE_REWARD_SUCCESS };
 }
