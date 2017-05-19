eb4419c788aa33018229e0f732785bceb866de55
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-05-19 23:51:38 +0200

    #175 feta

--- app/components/Map/index.js
+++ app/components/Map/index.js
@@ -6,6 +6,7 @@
 
 import React, { PropTypes } from 'react';
 import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer';
 import CustomMarker from '../CustomMarker';
 
 class Map extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -21,17 +22,23 @@ class Map extends React.Component { // eslint-disable-line react/prefer-stateles
         defaultZoom={8}
         center={this.state.center}
       >
-        {this.props.markers.map((marker) => (
-          <CustomMarker
-            key={marker.issue_auth_token}
-            marker={marker}
-            position={{ lat: marker.latitude, lng: marker.longitude }}
-            category={marker.category}
-            onClick={this.props.showIssue}
-            map={this}
-          >
-          </CustomMarker>
-        ))}
+        <MarkerClusterer
+          averageCenter
+          enableRetinaIcons
+          gridSize={30}
+        >
+          {this.props.markers.map((marker) => (
+            <CustomMarker
+              key={marker.issue_auth_token}
+              marker={marker}
+              position={{ lat: marker.latitude, lng: marker.longitude }}
+              category={marker.category}
+              onClick={this.props.showIssue}
+              map={this}
+            >
+            </CustomMarker>
+          ))}
+        </MarkerClusterer>
       </GoogleMap>
     );
   }
